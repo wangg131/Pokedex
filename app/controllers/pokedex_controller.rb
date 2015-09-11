234148da9ee22299032f83ae2e02b3b3b658c773
Loren Wang <wang131.131@gmail.com>
2015-09-11 16:50:01 -0700

    wip pseudo code

--- app/controllers/pokedex_controller.rb
+++ app/controllers/pokedex_controller.rb
@@ -5,7 +5,7 @@ class PokedexController < ApplicationController
   def index
     begin
       @number = params[:id]
-      response = HTTParty.get(POKESEARCH + "#{@number}")
+      response = HTTParty.get(POKESEARCH + "55")
       data = fetching_data(response)
       code = :ok
     rescue
@@ -19,11 +19,31 @@ class PokedexController < ApplicationController
   def show; end
 
   def fetching_data(response)
+          # response2 = Pokegem.get "pokemon", 25
     pokemons = response.fetch, {}
     pokemons.map do |pokemon|
       {
-        name: pokemon.fetch("name", "")
+        name: pokemon.fetch("name", ""),
+        national_id: pokemon.fetch("national_id", ""),
+        types: pokemon.fetch("types", []),
+          #@type = response["types"]
+          #@type.each do |type_hash|
+            #@type_array.push(type_hash["name"])
+            #@type_array
+        descriptions: pokemon.fetch("name", []),
+          # first hash["resource_uri"] >> ["description"].. can just chain it?
+        evolutions: pokemon.fetch("evolutions", [])
+          # level:
+          # to:
       }
     end
   end
 end
+#
+# r = HTTParty.get("http://pokeapi.co/api/v1/pokedex/1")
+# => {"created"=>"2013-11-09T15:14:48.957604",
+#  "modified"=>"2013-11-09T15:14:48.957565",
+#  "name"=>"national",
+#  "pokemon"=>
+#[{"name"=>"golduck_gen_1",
+#   pokemon_list = r["pokemon"]....[{"name"=>"rattata",
