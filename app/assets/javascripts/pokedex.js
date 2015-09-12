a8572eff21df93092eda55756829a3114ee38f7d
Loren Wang <wang131.131@gmail.com>
2015-09-11 21:27:32 -0700

    getting response back! woo

--- app/assets/javascripts/pokedex.js
+++ app/assets/javascripts/pokedex.js
@@ -1,3 +1,29 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(function() {
+
+  $(".search").click(function(event) {
+    event.preventDefault();
+
+// TODO rename classes of the divs appropriately
+    var searchButton = $(this);
+    var formTag = searchButton.parents(".form");
+    var text_box = formTag.children(".text_box");
+    var search_input = text_box.val();
+    var url = "/search/" + search_input;
+
+    $.ajax(url, {
+      type: GET,
+      success: pokemonStats
+    });
+
+  });
+
+  function pokemonStats(data) {
+
+
+  }
+
+});
--- app/controllers/pokedex_controller.rb
+++ app/controllers/pokedex_controller.rb
@@ -4,8 +4,8 @@ class PokedexController < ApplicationController
 
   def index
     begin
-      @number = params[:id]
-      response = HTTParty.get(POKESEARCH + "55")
+      @number = params[:pokemon]
+      response = HTTParty.get(POKESEARCH + "6")
       data = fetching_data(response)
       code = :ok
     rescue
@@ -13,37 +13,16 @@ class PokedexController < ApplicationController
       code = :no_content
     end
     render json: data.as_json, code: code
-
   end
 
   def show; end
 
   def fetching_data(response)
-          # response2 = Pokegem.get "pokemon", 25
-    pokemons = response.fetch, {}
-    pokemons.map do |pokemon|
-      {
-        name: pokemon.fetch("name", ""),
-        national_id: pokemon.fetch("national_id", ""),
-        types: pokemon.fetch("types", []),
-          #@type = response["types"]
-          #@type.each do |type_hash|
-            #@type_array.push(type_hash["name"])
-            #@type_array
-        descriptions: pokemon.fetch("name", []),
-          # first hash["resource_uri"] >> ["description"].. can just chain it?
-        evolutions: pokemon.fetch("evolutions", [])
-          # level:
-          # to:
-      }
-    end
+    return {
+      name: response.fetch("name"),
+      types: response.fetch("types").map do |type|
+          { name: type.fetch("name") }
+      end
+    }
   end
 end
-#
-# r = HTTParty.get("http://pokeapi.co/api/v1/pokedex/1")
-# => {"created"=>"2013-11-09T15:14:48.957604",
-#  "modified"=>"2013-11-09T15:14:48.957565",
-#  "name"=>"national",
-#  "pokemon"=>
-#[{"name"=>"golduck_gen_1",
-#   pokemon_list = r["pokemon"]....[{"name"=>"rattata",
