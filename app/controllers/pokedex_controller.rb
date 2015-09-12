93e5c76744993e28cbba67265fb3fe8d666064a1
Loren Wang <wang131.131@gmail.com>
2015-09-11 22:20:51 -0700

    refactoring fetching data from response

--- app/controllers/pokedex_controller.rb
+++ app/controllers/pokedex_controller.rb
@@ -20,17 +20,23 @@ class PokedexController < ApplicationController
   # end
 
   def fetching_data(response)
+    types = response.fetch("types").map { |type| { name: type.fetch("name") }}
+    descriptions = response.fetch("descriptions").map { |description| { name: description.fetch("name") }}
+    evolutions = response.fetch("evolutions").map do |evolution|
+      {
+        level: evolution.fetch("level"),
+        method: evolution.fetch("method"),
+        to: evolution.fetch("to")
+      }
+      end
+
     return {
       name: response.fetch("name"),
       id: response.fetch("national_id"),
-      types: response.fetch("types").map { |type| { name: type.fetch("name") }},
-      
-    }
+      types: types,
+      description: descriptions,
+      evolutions: evolutions
+      }
   end
 
-# evolutions: response.fetch("evolutions").map do |evolution|
-#   { level: evolution("level"),
-#     method: evolution("method"),
-#     to: evolution("to") }
-# end,
 end
