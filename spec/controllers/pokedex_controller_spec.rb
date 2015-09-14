e695fa033b9f87dee8d5a651c005c1f584eed33d
Loren Wang <wang131.131@gmail.com>
2015-09-13 22:57:46 -0700

    testing pokeapi controller

--- spec/controllers/pokedex_controller_spec.rb
+++ spec/controllers/pokedex_controller_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe PokedexController, type: :controller do
+POKEAPI = { cassette_name: "POKEAPI", record: :new_episodes }
 
+RSpec.describe PokedexController, type: :controller do
+  describe "interacting with the PokeAPi", vcr: POKEAPI do
+    before :each do
+      get :search_query, id: "55"
+    end
+
+    it "should be successful" do
+      expect(response).to be_ok
+    end
+
+    it "should return a json response object" do
+      expect(response.header['Content-Type']).to include 'application/json'
+    end
+
+    context "the returned json object" do
+      it "has the right keys" do
+        data = JSON.parse response.body
+
+        %w(id).each do |key|
+          expect(data.map(&:keys).flatten.uniq).to include key
+        end
+      end
+    end
+  end
 end
